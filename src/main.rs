87daae8ac3cb5b38da0e74e9f96aa27feb7c5d00
broge2 <broge2@uis.edu>
2023-05-01 12:23:01 -0500

    Turn into functions and allow user input

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
-use nalgebra::Vector2;
 use std::sync::atomic::{AtomicUsize, Ordering};
+use std::io::Write;
+use std::io;
+
+use nalgebra::Vector2;
 use rayon::prelude::*;
 
-const MAX_DARTS: usize = 100_000_000;
+fn get_input() -> io::Result<usize> {
+    let mut userin = String::new();
+    print!("Number of darts to throw: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut userin)?;
+    Ok(userin.trim().parse::<usize>().unwrap_or(0))
+}
 
-fn main() {
-
+fn throw_darts(max_darts: usize) -> usize {
     let mut darts_that_hit_sphere: AtomicUsize = AtomicUsize::new(0);
-    (1..=MAX_DARTS).into_par_iter().for_each(|_| {
+    (1..=max_darts).into_par_iter().for_each(|_| {
         let dart: Vector2<f32> = Vector2::new_random();
         if dart.magnitude() <= 1.0 {
             darts_that_hit_sphere.fetch_add(1, Ordering::Relaxed);
         }
     });
 
-    let ratio = *darts_that_hit_sphere.get_mut() as f64 / MAX_DARTS as f64;
-    let pi = (4.0) * ratio;
+    *darts_that_hit_sphere.get_mut()
+}
 
-    println!("pi = {pi}");
+fn main() -> io::Result<()> {
+    let max_darts = get_input().unwrap();
+    if max_darts > 0 {
+        println!("Throwing {max_darts} darts");
+        let darts_that_hit = throw_darts(max_darts);    
+
+        let ratio = darts_that_hit as f64 / max_darts as f64;
+        let pi = (4.0) * ratio;
+
+        println!("pi = {pi}");
+    } else {
+        println!("No darts thrown.");
+    }
+
+    Ok(())
 }
