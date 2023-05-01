8850697a63db6d1ef5ad8e540a56fed7d12a0c9f
broge2 <broge2@uis.edu>
2023-05-01 11:45:29 -0500

    Working multithreaded solution

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use nalgebra::Vector2;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use rayon::prelude::*;
+
+const MAX_DARTS: usize = 100_000_000;
+
+fn hit_sphere(point: Vector2<f32>) -> bool {
+    point.magnitude() <= 1.0
+}
+
+fn main() {
+
+    let mut darts_that_hit_sphere: AtomicUsize = AtomicUsize::new(0);
+    (1..=MAX_DARTS).into_par_iter().for_each(|_| {
+        let dart: Vector2<f32> = Vector2::new_random();
+        if hit_sphere(dart) {
+            darts_that_hit_sphere.fetch_add(1, Ordering::Relaxed);
+        }
+    });
+
+    let ratio = *darts_that_hit_sphere.get_mut() as f64 / MAX_DARTS as f64;
+    let pi = (4.0) * ratio;
+
+    println!("pi = {pi}");
+}
